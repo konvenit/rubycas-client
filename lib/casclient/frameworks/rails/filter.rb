ece252b99238d3c1de7a57dd8b60535fc49791f4
Frederik Fix <ich@derfred.com>
2014-09-09 16:13:22 +0200

    mega fick scheiss arschloch drecks schweinerein

--- lib/casclient/frameworks/rails/filter.rb
+++ lib/casclient/frameworks/rails/filter.rb
@@ -53,10 +53,10 @@ module CASClient
           params = controller.params.dup
           params.delete(:ticket)
           params.delete(:format) if params[:format].to_s == 'html'
-Rails.logger.info params.inspect
+::Rails.logger.info params.inspect
 
           service_url = controller.url_for(params)
-          Rails.logger.info("Guessed service url: #{service_url.inspect}")
+          ::Rails.logger.info("Guessed service url: #{service_url.inspect}")
           return service_url
         end
 
