e50cd52fe7500ac28e2a84e4b4cfa367ddaae8c9
amin19948 <m.aldirani@miceportal.com>
2023-08-10 11:39:54 +0200

    Update filter.rb

--- lib/casclient/frameworks/rails/filter.rb
+++ lib/casclient/frameworks/rails/filter.rb
@@ -80,11 +80,11 @@ module CASClient
         # ways.
         # If given, the optional <tt>service</tt> URL overrides
         # <tt>request.referer</tt>.
-        def self.logout(controller, service = nil, options)
+        def self.logout(controller, service = nil)
           referer = service || controller.request.referer
           st = controller.session[:cas_last_valid_ticket]
           controller.send(:reset_session)
-          controller.send(:redirect_to, client.logout_url(referer), options)
+          controller.send(:redirect_to, client.logout_url(referer), allow_other_host: true)
         end
 
         def self.redirect_to_cas_for_authentication(controller)
