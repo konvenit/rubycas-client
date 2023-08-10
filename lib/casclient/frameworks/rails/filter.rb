ba46c5564c596cac589260347c6187fa2b9f33f8
amin19948 <m.aldirani@miceportal.com>
2023-08-10 14:52:41 +0200

    Update filter.rb

--- lib/casclient/frameworks/rails/filter.rb
+++ lib/casclient/frameworks/rails/filter.rb
@@ -84,7 +84,7 @@ module CASClient
           referer = service || controller.request.referer
           st = controller.session[:cas_last_valid_ticket]
           controller.send(:reset_session)
-          controller.send(:redirect_to, client.logout_url(referer))
+          controller.send(:redirect_to, client.logout_url(referer), allow_other_host: true)
         end
 
         def self.redirect_to_cas_for_authentication(controller)
